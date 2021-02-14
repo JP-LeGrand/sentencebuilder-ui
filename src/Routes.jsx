d1c3c39c17da60343074e6a2bf88f4471a88512a
josephlegrand291 <josephlegrand@291gmail.com>
2021-02-14 19:24:56 +0200

    Removed unused Module

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import History from "./pages/History";
 
