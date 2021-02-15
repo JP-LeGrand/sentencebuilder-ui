5ba66d455ce06c8073c0a86ebebbc668d53b87d6
josephlegrand291 <josephlegrand@291gmail.com>
2021-02-15 14:18:29 +0200

    Updated url for api when it is in production

--- src/api/sentenceBuilderApi.js
+++ src/api/sentenceBuilderApi.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+const apiUrl = () => {
+  let url = "";
+  if (window.origin === "http://localhost:3000") {
+    url = `http://localhost:3001/`;
+  } else {
+    url = `https://confident-babbage-ad385a.netlify.app/.netlify/functions/server/`;
+  }
+  return url;
+};
+
 export default axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: apiUrl(),
 });
